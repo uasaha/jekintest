c565f85f7ea4ef295750c931c44cbb8b652361a1
Woon <81662836+uasaha@users.noreply.github.com>
2023-01-02 17:02:28 +0900

    Add files via upload

---
+++ src/test/java/com/example/jenkintest/JenkintestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkintest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkintestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
